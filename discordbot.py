433f755e43a1cb2cf8eb5d1f8b18e2adecbe0e88
Keito <54165015+Tanesan@users.noreply.github.com>
2021-03-14 14:43:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -47,7 +47,7 @@ async def nomal_reply(message):
     emoji = discord.utils.get(message.guild.emojis, name=choice(EMOJI))
     reply_message = clients.talk(message)
     reply = reply_message['results'][0]['reply']
-    if reply.find('ごめんなさい。') > 0:
+    if reply.find('ごめんなさい。') > -1:
         reply = f'どうでも良いことに関するお問い合わせは、別の42staffの方にお問い合わせください。'
     await message.channel.send(reply)
     await message.add_reaction(emoji)
